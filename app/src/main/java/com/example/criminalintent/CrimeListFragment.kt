738d65f7ea31a8b25b3afabfe636a6e775d88370
Hommaster <ilya.pan.2017@mail.ru>
2024-01-12 15:16:19 +0300

    pre-alpha v.0.1.3a

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -1,9 +1,15 @@
 package com.example.criminalintent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CrimeListFragment: Fragment() {
 
@@ -11,9 +17,24 @@ class CrimeListFragment: Fragment() {
         ViewModelProvider(this)[CrimeListViewModel::class.java]
     }
 
+    private lateinit var crimeRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(Constance.TAG_CLF, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view)
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
     companion object {
